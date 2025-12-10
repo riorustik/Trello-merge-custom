3301bd797cdd28f365556eaab61bda0c0ad869e9
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-10 19:59:29 +0400

    Breaking into functions | cleaning code

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-
 const placeholders = document.querySelectorAll('.placeholder');
-let o = {}
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+const addBtn = document.querySelector('.add-task-button');
 let item = document.querySelectorAll('.item');
-console.log(item)
-lisItem();
-function lisItem() {
+let currentObjSaveEventTarget = {}
 
-    item.forEach((item) => {
-        item.addEventListener('dragstart', dragstart);
-        item.addEventListener('dragend', dragend);
-    })
-}
+lisItem();
+
 placeholders.forEach(placeholder => {
     placeholder.addEventListener('dragover', dragover);
     placeholder.addEventListener('dragenter', dragenter);
     placeholder.addEventListener('dragleave', dragleave);
     placeholder.addEventListener('drop', drop);
-    placeholder.addEventListener('click', function (e) {
-        // if (e.target.tagName === 'LI') {
-        //     // e.target.classList.toggle('checked');
-        //     saveData()
-        // } else if (e.target.tagName === 'SPAN') {
-        e.target.parentElement.remove();
-        //     saveData()
-        // }
-    }, false);
 })
 
+addBtn.addEventListener('click', () => {
+    if (inputBox.value.trim() === '') {
+        alert('Please enter a valid task');
+    } else {
+        const newItem = createItem()
+        listContainer.append(newItem);
+
+        const deleteBtn = removeItem();
+        newItem.append(deleteBtn);
+
+        item = document.querySelectorAll('.item');
+        lisItem();
+    }
+    inputBox.value = '';
+})
+
+function lisItem() {
+    item.forEach((item) => {
+        item.addEventListener('dragstart', dragstart);
+        item.addEventListener('dragend', dragend);
+    })
+}
+
 function dragstart(e) {
-    // lisItem()
-    // console.log(e.target)
     e.target.classList.add('hold');
     setTimeout(() => e.target.classList.add('hide'), 0);
-
-    o.key = e.target;
+    currentObjSaveEventTarget.key = e.target;
 }
 
 function dragend(e) {
@@ -43,72 +50,42 @@ function dragend(e) {
 function dragover(e) {
     e.preventDefault();
 }
+
 function dragenter(e) {
     e.target.classList.add('hovered');
 }
+
 function dragleave(e) {
     e.target.classList.remove('hovered');
 }
+
 function drop(e) {
-    let s = o.key
-
-    if(e.target.tagName === 'LI'){
+    let elem = currentObjSaveEventTarget.key
+    if (e.target.tagName === 'LI') {
         e.target.classList.remove('hovered');
-        console.log(e.target)
-        console.log(s);
-        e.target.parentElement.append(s)
-    }
-    else {
-        console.log(e.target)
-        console.log(s);
-        e.target.classList.remove('hovered');
-        e.target.append(s)
-
-    }
-}
-
-
-
-const inputBox = document.getElementById('input-box');
-const listContainer = document.getElementById('list-container');
-
-function addTask() {
-    if (inputBox.value === '') {
-        alert('Please enter a valid task');
+        e.target.parentElement.append(elem)
     } else {
-        let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
-        li.classList.add('item');
-        li.setAttribute('draggable', 'true');
-        listContainer.appendChild(li);
-        let span = document.createElement('span');
-        span.innerHTML = '\u00d7';
-        span.classList.add('delete');
-        li.appendChild(span);
-        item = document.querySelectorAll('.item');
-        lisItem();
-        console.log(item);
+        e.target.classList.remove('hovered');
+        e.target.append(elem)
     }
-    inputBox.value = '';
-    saveData();
 }
 
-listContainer.addEventListener('click', function (e) {
-    // if (e.target.tagName === 'LI') {
-    //     // e.target.classList.toggle('checked');
-    //     saveData()
-    // } else if (e.target.tagName === 'SPAN') {
-        e.target.parentElement.remove();
-    //     saveData()
-    // }
-}, false);
-
-function saveData() {
-    localStorage.setItem('data', listContainer.innerHTML);
+function createItem() {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    li.classList.add('item');
+    li.setAttribute('draggable', 'true');
+    return li;
 }
 
-function showTask() {
-    listContainer.innerHTML = localStorage.getItem('data');
+function removeItem() {
+    let span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    span.classList.add('delete');
+    span.addEventListener('click', (e) => {
+        if (e.target.tagName === 'SPAN') {
+            e.target.parentElement.remove();
+        }
+    });
+    return span;
 }
-
-// showTask();
